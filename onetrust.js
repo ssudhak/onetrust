0270104acdd716e45ae0c794cba8dfc6f14dbbdf
ssudhak <sudheesh.sudhakaran@gmail.com>
2022-06-23 10:53:33 -0400

    Create onetrust.js
    one trust changes

---
+++ onetrust.js
@@ -0,0 +1,8 @@
+function dynamicallyLoadScript(url) {
+  var script = document.createElement("script");  // create a script DOM node
+  script.src = url;  // set its src to the provided URL
+   
+  document.head.appendChild(script);  // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+}
+
+dynamicallyLoadScript('')
